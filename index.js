fccc700145ba629dd86e505dbd625ff3ebf0cbac
Steven Spencer <steve35spencer@gmail.com>
2020-05-07 13:38:23 -0400

    deploy for project

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const server = require('./server.js')
 
-const port = process.env.PORT || 4422;
-
+const port = process.env.PORT || 5555;
 server.listen(port, () => {
-    console.log("\n *Server Running on http://localhost:4422 \n")
-})
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
